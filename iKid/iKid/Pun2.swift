aeee393882a8bf4fca0c434729530f73c9c1cc43
David Yuan <davidnyuan@gmail.com>
2016-04-28 16:55:25 -0700

    completed assignment

---
+++ iKid/iKid/Pun2.swift
@@ -0,0 +1,14 @@
+//
+//  Pun1.swift
+//  iKid
+//
+//  Created by David Yuan on 4/28/16.
+//  Copyright © 2016 David Yuan. All rights reserved.
+//
+
+
+import UIKit
+
+class Pun2: UIViewController {
+    
+}
